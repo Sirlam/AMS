b13cab3f51ea90264af5dbc0c1b5fd98150f5501
Salam Olanrewaju <salam_lanre@yahoo.com>
2018-08-08 12:18:12 +0100

    Created basic view and blade templates

---
+++ resources/views/layout/front/footer.blade.php
@@ -0,0 +1,32 @@
+<!-- Footer -->
+<footer class="footer-main">
+	&copy; 2018 <strong>AMS</strong> - Account Management System</a>
+</footer>
+<!-- /footer -->
+
+  </div>
+  <!-- /main content -->
+
+</div>
+<!-- /main container -->
+
+</div>
+<!-- /page container -->
+
+<!--Load JQuery-->
+<script type="text/javascript" src="{{URL::asset('js/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/metismenu/jquery.metisMenu.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/blockui-master/jquery-ui.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/blockui-master/jquery.blockUI.js')}}"></script>
+<!--Float Charts-->
+<script type="text/javascript" src="{{URL::asset('js/plugins/flot/jquery.flot.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/flot/jquery.flot.resize.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/flot/jquery.flot.selection.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/flot/jquery.flot.pie.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/plugins/flot/jquery.flot.time.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js/functions.js')}}"></script>
+
+<!--ChartJs-->
+<script type="text/javascript" src="{{URL::asset('js/plugins/chartjs/Chart.min.js')}}"></script>
